755d30b72fa0d45d65aa23a96d5e8a2d2a0189e3
바키찬 <ckstmznf@naver.com>
2025-11-11 15:29:08 +0900

    Feat: Initial Commit

---
+++ arduino.py
@@ -0,0 +1,65 @@
+import time
+from typing import Iterable
+
+import pyfirmata2
+
+class Arduino:
+    def __init__(self, PORT: str):
+        self.PORT = PORT
+        self.board = pyfirmata2.Arduino(PORT)
+
+        self.SAMPLING_INTERVAL = 100
+        self.LED_ON = 0
+        self.LED_OFF = 1
+        self.LED_PINS = [10, 11, 12, 13]
+
+        self.board.samplingOn(self.SAMPLING_INTERVAL)
+
+        self.is_prev_clicked: dict[str, bool] = {}
+
+    def __del__(self):
+        if (self.board):
+            self.board.exit();
+
+    def led_on(self, pins: Iterable[int], gap: float = 0):
+        for i in pins:
+            self.board.digital[i].write(self.LED_ON)
+            time.sleep(gap)
+
+    def led_off(self, pins: Iterable[int], gap: float = 0):
+        for i in pins:
+            self.board.digital[i].write(self.LED_OFF)
+            time.sleep(gap)
+
+    def all_led_on(self, gap: float = 0):
+        self.led_on(pins=self.LED_PINS, gap=gap)
+
+    def all_led_off(self, gap: float = 0):
+        self.led_off(pins=self.LED_PINS, gap=gap)
+
+    def lef_blink(self, pins: Iterable[int], repeat: int, gap: float):
+        self.led_off(pins=pins)
+
+        for _ in range(repeat):
+            self.led_on(pins)
+            time.sleep(gap)
+
+            self.led_off(pins)
+            time.sleep(gap)
+
+    def add_on_click_event(self, pin: str, call_back):
+        def clicked(v):
+            is_pressed = v < 0.5
+            if (is_pressed):
+                self.is_prev_clicked[pin] = True
+            else:
+                if (not self.is_prev_clicked[pin]):
+                    return
+
+                self.is_prev_clicked[pin] = False
+                call_back()
+
+        self.is_prev_clicked[pin] = False
+        analog_1 = self.board.get_pin(pin)
+        analog_1.register_callback(clicked)
+        analog_1.enable_reporting()
---
+++ main.py
@@ -0,0 +1,23 @@
+import numpy as np
+from arduino import Arduino
+
+if __name__ == '__main__':
+    uno = Arduino(
+        PORT="COM6"
+    )
+
+    click_count = np.array([0, 0, 0])
+
+
+    def a1u_click_callback(index: int):
+        global click_count, uno
+        click_count[index] += 1
+        print(f"{click_count}번 클릭")
+
+        if(click_count[0] > 10):
+            uno.lef_blink(uno.LED_PINS, 5, 0.2)
+
+
+    uno.add_on_click_event("a:1:u", lambda: a1u_click_callback(0))
+
+    input()
