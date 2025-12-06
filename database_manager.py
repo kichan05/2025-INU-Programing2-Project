7b9ac2917d7f7228483ec0a4d79c0d1453f2da1d
바키찬 <ckstmznf@naver.com>
2025-12-06 13:49:33 +0900

    DatabaseManager 클래스 작성

---
+++ database_manager.py
@@ -0,0 +1,58 @@
+import datetime
+import sqlite3
+
+
+class DatabaseManager:
+    def __init__(self):
+        self.conn = sqlite3.connect("game_history.db", check_same_thread=False)
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS history (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                player_name TEXT, 
+                                reaction_time REAL, 
+                                played_at TEXT,
+                                game_mode TEXT,
+                                score INTEGER
+                                )''')
+        self.conn.commit()
+
+    def save_record(self, game_mode, name, r_time, score):
+        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.cursor.execute(
+            f"""INSERT INTO history
+                    (game_mode, player_name, reaction_time, score, played_at)
+                VALUES
+                    (?, ?, ?, ?, ?)
+            """,
+            (game_mode, name, r_time * 1000, score, now)
+        )
+        self.conn.commit()
+
+    def get_stats_by_player(self, game_mode, player_name):
+        self.cursor.execute(
+            """SELECT
+                played_at, score
+            FROM
+                history
+            WHERE
+                game_mode = ? AND player_name = ?
+            """,
+            (game_mode, player_name)
+        )
+        return self.cursor.fetchall()
+
+    def get_all_player_names(self):
+        self.cursor.execute(
+            """
+                SELECT
+                    DISTINCT player_name
+                FROM
+                    history
+                ORDER BY
+                    player_name ASC
+            """
+        )
+        return [row[0] for row in self.cursor.fetchall()]
+
+    def close(self):
+        self.conn.close();
