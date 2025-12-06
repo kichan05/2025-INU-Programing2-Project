3fe5060aa166429212caa91d98673a4ac3833950
바키찬 <ckstmznf@naver.com>
2025-12-06 23:31:40 +0900

    Revert "Feat: ArduiniManager 클래스 제작"
    This reverts commit 0b8306ec239163773c200d2d845c3a3ae0b3fb0d.

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 import tkinter as tk
-
-import pyfirmata2
-
 from app import ReactionGameApp
-from arduino_manager import ArduinoManager
 
 if __name__ == "__main__":
-    # root = tk.Tk()
-    # app = ReactionGameApp(root)
-    # root.protocol("WM_DELETE_WINDOW", app.on_closing)
-    # root.mainloop()
-
-    def on_click():
-        print("Hello World")
-
-    uno = ArduinoManager(PORT=pyfirmata2.Arduino.AUTODETECT)
-    uno.on_click("a:1:i", on_click)
-
-    input()
+    root = tk.Tk()
+    app = ReactionGameApp(root)
+    root.protocol("WM_DELETE_WINDOW", app.on_closing)
+    root.mainloop()
